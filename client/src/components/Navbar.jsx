0dda8b9f926db32b7bdca0e90f8ccd852873367a
Noobieprogrammer69 <joververgara1@gmail.com>
2022-07-17 14:59:36 +0800

    make it better

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 import { Link, useLocation } from 'react-router-dom';
 
-import IconButton from '@material-ui/icons/IconButton';
+import IconButton from '@material-ui/core/IconButton';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import HomeIcon from '@material-ui/icons/Home';
 import MessageIcon from '@material-ui/icons/Message';
